62050105f857c5e46e03b437db6e0272956c2420
Grover Cristobal <elvis.cristobal.garrido@gmail.com>
2023-05-01 17:46:24 -0500

    10. Definir función en un coponente

--- src/App.js
+++ src/App.js
@@ -4,28 +4,37 @@ import freecodeCampLogo from './images/freecodecamp-logo.png'
 import Boton from './components/Boton';
 
 function App() {
-  return (
-    <div className="App">
-        <div className='freecodecamp-logo-contenedor'>
-            <img 
-                className='freecodecamp-logo' 
-                src={freecodeCampLogo}  // nuevo modo de enlazar una imagen
-                alt='Logo de freeCodeCamp' 
-            />
+
+    const manejarClic = () => {
+        console.log('Clic')
+    }
+
+    const reiniciarContador = () => {
+        console.log('Reiniciar')
+    }
+
+    return (
+        <div className="App">
+            <div className='freecodecamp-logo-contenedor'>
+                <img 
+                    className='freecodecamp-logo' 
+                    src={freecodeCampLogo}  // nuevo modo de enlazar una imagen
+                    alt='Logo de freeCodeCamp' 
+                />
+            </div>
+            <div className='contenedor-principal'>
+                {/* aquí crearemos nuestros componentes: el Contador y los Botomes */}
+                <Boton 
+                    texto='Clic'
+                    esBotonDeClic={true}
+                    manejarClic={} />
+                <Boton 
+                    texto='Reiniciar'
+                    esBotonDeClic={false}
+                    manejarClic={} />
+            </div>
         </div>
-        <div className='contenedor-principal'>
-            {/* aquí crearemos nuestros componentes: el Contador y los Botomes */}
-            <Boton 
-                texto='Clic'
-                esBotonDeClic={true}
-                manejarClic={} />
-            <Boton 
-                texto='Reiniciar'
-                esBotonDeClic={false}
-                manejarClic={} />
-        </div>
-    </div>
-  );
+    );
 }
 
 export default App;
