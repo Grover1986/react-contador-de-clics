26a536080caa8ddaba4e754d494bbc391de86c13
Grover Cristobal <elvis.cristobal.garrido@gmail.com>
2023-05-01 16:39:47 -0500

    7. Clases basadas en condición

--- src/components/Boton.js
+++ src/components/Boton.js
@@ -1,8 +1,10 @@
 import React from "react";
 
-function Boton({ texto }) {
+function Boton({ texto, esBotonDeClic }) { //agregamos otro prop 'esBotonDeClic'
     return (
-        <button>
+        /* este botón será dinámico ya q dirá 'Hacer clic' pero a veces tmb cambiará a 'Reiniciar', asi q para eso tenemos q hacer q sus clases sean 
+            dinámicas, para eso manejaremos el operador ternario */
+        <button className={esBotonDeClic ? 'boton-clic' : 'boton-reiniciar'}>
             {texto}
         </button>
     )
