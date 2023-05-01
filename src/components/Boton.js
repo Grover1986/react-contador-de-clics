a311266ea754a70592eed0795aac372cc504a0e9
Grover Cristobal <elvis.cristobal.garrido@gmail.com>
2023-05-01 16:51:21 -0500

    8. Asignando eventListener

--- src/components/Boton.js
+++ src/components/Boton.js
@@ -1,11 +1,15 @@
 import React from "react";
 
-function Boton({ texto, esBotonDeClic }) { //agregamos otro prop 'esBotonDeClic'
+// agregamos una función 'manejarClic' como prop
+function Boton({ texto, esBotonDeClic, manejarClic }) { //agregamos otro prop 'esBotonDeClic'
     return (
         /* este botón será dinámico ya q dirá 'Hacer clic' pero a veces tmb cambiará a 'Reiniciar', asi q para eso tenemos q hacer q sus clases sean 
             dinámicas, para eso manejaremos el operador ternario */
-        <button className={esBotonDeClic ? 'boton-clic' : 'boton-reiniciar'}>
+        <button className={esBotonDeClic ? 'boton-clic' : 'boton-reiniciar'}
+        onClick={manejarClic}>
             {texto}
         </button>
     )
 }
+
+export default Boton;
