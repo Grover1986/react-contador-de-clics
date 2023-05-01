6cb082c9b6be3eb03920e5c98fbb638177bcf082
Grover Cristobal <elvis.cristobal.garrido@gmail.com>
2023-05-01 13:39:18 -0500

    2. Otra alternativa para agregar imágenes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+// importando imagen
+import freecodeCampLogo from './images/freecodecamp-logo.png'
 
 function App() {
   return (
     <div className="App">
-        
+        <div className='freecodecamp-logo-contenedor'>
+            <img 
+                className='freecodecamp-logo' 
+                src={freecodeCampLogo}  // nuevo modo de enlazar una imagen
+                alt='Logo de freeCodeCamp' 
+            />
+        </div>
     </div>
   );
 }
